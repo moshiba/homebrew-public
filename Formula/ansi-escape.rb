1d66a921bd51c418e8c00b41f1e5b8df9f7decda
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-11-12 12:42:25 +0800

    ansi-escape release v1.1.2

--- Formula/ansi-escape.rb
+++ Formula/ansi-escape.rb
@@ -10,35 +10,42 @@ class AnsiEscape < Formula
   head "https://github.com/hsuantinglu/ansi-escape.git"
 
   stable do
-    url "https://github.com/hsuantinglu/ansi-escape/archive/v1.0.0.tar.gz"
-    sha256 "2f92ee01d8266ca8b1dc6e3d5a19acfc47cf80a4a900b692c577ba6ee2e46b63"
+    version "1.1.1"
+    url "https://github.com/hsuantinglu/ansi-escape/archive/v1.1.2.tar.gz"
+    sha256 "a3b189b9353eecd2da6623d309e31fd4ecefae9cf101d239117f5d0bb583721c"
 
-    #patch do
-    #  url ""
-    #  sha256 ""
-    #end
+    # patch do
+    #   url ""
+    #   sha256 ""
+    # end
   end
 
-  #bottle do
-  #  sha256 "" => :catalina
-  #  sha256 "" => :mojave
-  #end
+  # bottle do
+  #   cellar :any
+  #   sha256 "" => :catalina
+  # end
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make"
-    system "make", "install"
+    mkdir "_build" do
+      args = std_cmake_args + %w[
+        -DRELEASE=ON
+      ]
+
+      system "cmake", "..", *args, "-DBUILD_SHARED_LIBS=ON"
+      system "make"
+      system "make", "install"
+
+      #TODO: build a static library too
+    end
   end
 
-  #def post_install
-  #end
+  # def post_install
+  # end
 
   test do
     ohai "Setting up ansi-escape repository in " + testpath
     ohai "There's no test for this repo, for now."
-    system "false"
-    ohai "Test complete."
   end
 end
