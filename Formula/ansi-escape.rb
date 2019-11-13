3b511c7c842d4c4b9b7c50a60caa55c23e889491
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-11-13 18:56:19 +0800

    ansi-escape 1.2.1

--- Formula/ansi-escape.rb
+++ Formula/ansi-escape.rb
@@ -5,19 +5,19 @@
 #
 
 class AnsiEscape < Formula
-  AESC_VERSION = "1.1.2".freeze
+  AESC_VERSION = "1.2.1".freeze
   desc "ANSI escape codes wrapped in C++ string streams"
   homepage "https://hsuantinglu.github.io/ansi-escape/"
   version "#{AESC_VERSION}"
   url "https://github.com/hsuantinglu/ansi-escape/archive/v#{AESC_VERSION}.tar.gz"
-  sha256 "a3b189b9353eecd2da6623d309e31fd4ecefae9cf101d239117f5d0bb583721c"
+  sha256 "23278fa840b390f5ab9f861f085c02605a314557fd19472cc7d2bf4c49aa205d"
   head "https://github.com/hsuantinglu/ansi-escape.git"
 
-  bottle do
-    root_url "https://github.com/HsuanTingLu/ansi-escape/releases/download/v#{AESC_VERSION}"
-    cellar :any
-    sha256 "e4a86452998fb73f8c6c6c5c16c4c84b66badd2f9687d076490a0c7a4bc9badf" => :catalina
-  end
+  # bottle do
+  #   root_url "https://github.com/HsuanTingLu/ansi-escape/releases/download/v#{AESC_VERSION}"
+  #   cellar :any
+  #   sha256 "e4a86452998fb73f8c6c6c5c16c4c84b66badd2f9687d076490a0c7a4bc9badf" => :catalina
+  # end
 
   depends_on "cmake" => :build
 
